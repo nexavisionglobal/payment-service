43c7540a7aaa8b1364b6b0d342d00d295f1e09ae
nexavisionglobal <nexavision.global@gmail.com>
2026-07-19 13:19:11 -0400

    Create StripeService for Stripe payment integration

---
+++ src/main/java/com/kodegravity/paymentservice/service/StripeService.java
@@ -0,0 +1,35 @@
+package com.kodegravity.paymentservice.service;
+
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import com.stripe.param.PaymentIntentCreateParams;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StripeService {
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+
+    // CREATE PAYMENT INTENT
+    public String createPaymentIntent(Long amount, String currency) throws StripeException {
+        Stripe.apiKey = stripeApiKey;
+
+        PaymentIntentCreateParams params =
+                PaymentIntentCreateParams.builder()
+                        .setAmount(amount)
+                        .setCurrency(currency)
+                        .build();
+
+        PaymentIntent intent = PaymentIntent.create(params);
+        return intent.getId();
+    }
+
+    // CONFIRM PAYMENT INTENT
+    public PaymentIntent confirmPaymentIntent(String paymentIntentId) throws StripeException {
+        Stripe.apiKey = stripeApiKey;
+        return PaymentIntent.retrieve(paymentIntentId);
+    }
+}
